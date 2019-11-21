8e46fa395bf6080d58bdb1a157915d40b7e6c233
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-21 21:10:44 +0200

    Implement contact form

--- assets/js/ed.js
+++ assets/js/ed.js
@@ -21,3 +21,47 @@
     checkbox.checked = false;
   }, false);
 })(document);
+
+(function ($) {
+  // Override the submit event
+  $(document).on('submit', '#contact-form', function (e) {
+    // Clear previous classes
+    $('.contact-form-item-error')
+      .removeClass('contact-form-item-error');
+
+    // Get form elements
+    let emailField   = $('.contact-form-input[name="email"]'),
+        nameField    = $('.contact-form-input[name="name"]'),
+        messageField = $('.contact-form-textarea[name="message"]'),
+        gotchaField  = $('.contact-form-gotcha');
+
+    // Validate email
+		if (nameField.val().length === 0) {
+      nameField
+        .closest('.contact-form-item')
+        .addClass('contact-form-item-error');
+    }
+
+    // Validate name
+		if (emailField.val().length === 0) {
+      emailField
+        .closest('.contact-form-item')
+        .addClass('contact-form-item-error');
+    }
+
+    // Validate message
+		if (messageField.val().length === 0) {
+      messageField
+        .closest('.contact-form-item')
+        .addClass('contact-form-item-error');
+    }
+
+    // If all fields are filled, except gotcha
+    if (emailField.val().length === 0   ||
+        nameField.val().length === 0    ||
+        messageField.val().length === 0 ||
+        gotchaField.val().length !== 0) {
+      e.preventDefault();
+    }
+  });
+}(jQuery));
