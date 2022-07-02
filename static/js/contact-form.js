ee5244df39f855b1eec20defb7f2eb05e5a4676a
Serghei Iakovlev <egrep@protonmail.ch>
2022-07-03 00:18:10 +0200

    Correct code style

--- static/js/contact-form.js
+++ static/js/contact-form.js
@@ -1,16 +1,16 @@
-$( document ).ready(function() {
-    const formElement = $("#contact-form");
-    const buttonElement = $("#send-form-button");
+$(function() {
+  const formElement = $('#contact-form');
+  const buttonElement = $('#send-form-button');
 
-    formElement.on("botpoison-challenge-start", function () {
-        buttonElement.attr("disabled", "disabled");
-    });
+  formElement.on('botpoison-challenge-start', function () {
+    buttonElement.attr('disabled', 'disabled');
+  });
 
-    formElement.on("botpoison-challenge-success", function () {
-        buttonElement.removeAttr("disabled");
-    });
+  formElement.on('botpoison-challenge-success', function () {
+    buttonElement.removeAttr('disabled');
+  });
 
-    formElement.on("botpoison-challenge-error", function () {
-        buttonElement.removeAttr("disabled");
-    });
+  formElement.on('botpoison-challenge-error', function () {
+    buttonElement.removeAttr('disabled');
+  });
 });
